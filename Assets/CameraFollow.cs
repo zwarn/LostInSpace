7f6ea148d65a56ccd0e58f4c0d146b44065c53c4
zwarn <michael.budahn@gmail.com>
2014-10-25 16:39:28 +0200

    Movement

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject toFollow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(toFollow.transform.position.x, toFollow.transform.position.y, -10);
+		transform.rotation = toFollow.transform.rotation;
+	}
+}
---
+++ Assets/SpaceShipScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceShipScript : MonoBehaviour {
+
+	public float rotationSpeed;
+	public float acceleration;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButton ("Up")) {
+			rigidbody2D.AddForce(transform.up * acceleration);
+		}
+
+		transform.Rotate (Vector3.forward * Input.GetAxis("Horizontal") * rotationSpeed);
+	
+	}
+}
